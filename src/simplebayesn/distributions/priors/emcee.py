2de089101273e8f982cea32ca38bf024d06fc170
Marco Giunta <72098193+marco-giunta@users.noreply.github.com>
2026-02-06 13:33:22 +0100

    add sigmac_int2 invgamma prior for emcee sampler

--- src/simplebayesn/distributions/priors/emcee.py
+++ src/simplebayesn/distributions/priors/emcee.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import invgamma
 
 def uniform_marginal_log_prior(global_params):
     return -np.inf if np.any(np.array([
@@ -7,3 +8,10 @@ def uniform_marginal_log_prior(global_params):
         global_params['sigmax2'],
         global_params['tau']
     ]) <= 0) else 0
+
+def uniform_marginal_log_prior_invgamma_sigmac_int2(global_params):
+    return -np.inf if np.any(np.array([
+        global_params['sigma_int2'],
+        global_params['sigmax2'],
+        global_params['tau']
+    ]) <= 0) else invgamma.logpdf(global_params['sigmac_int2'], a=0.003, scale=0.003)
