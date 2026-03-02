732997c7f17f1b2212dc5df0cfa8882c8f73fc81
Marco Giunta <72098193+marco-giunta@users.noreply.github.com>
2026-03-02 11:42:48 +0100

    add invgamma prior for all positive params

--- src/simplebayesn/distributions/priors/emcee.py
+++ src/simplebayesn/distributions/priors/emcee.py
@@ -12,6 +12,20 @@ def uniform_marginal_log_prior(global_params):
 def uniform_marginal_log_prior_invgamma_sigmac_int2(global_params, alpha: float = 0.003, beta: float = 0.003):
     return -np.inf if np.any(np.array([
         global_params['sigma_int2'],
+        global_params['sigmac_int2'],
         global_params['sigmax2'],
         global_params['tau']
     ]) <= 0) else invgamma.logpdf(global_params['sigmac_int2'], a=alpha, scale=beta)
+
+def uniform_invgamma_marginal_log_prior(global_params, alpha: float = 0.003, beta: float = 0.003):
+    return -np.inf if np.any(np.array([
+        global_params['sigma_int2'],
+        global_params['sigmac_int2'],
+        global_params['sigmax2'],
+        global_params['tau']
+    ]) <= 0) else np.sum((
+        invgamma.logpdf(global_params['sigma_int2'], a=alpha, scale=beta),
+        invgamma.logpdf(global_params['sigmac_int2'], a=alpha, scale=beta),
+        invgamma.logpdf(global_params['sigmax2'], a=alpha, scale=beta),
+        invgamma.logpdf(global_params['tau'], a=alpha, scale=beta),
+    ))
--- src/simplebayesn/distributions/priors/gibbs.py
+++ src/simplebayesn/distributions/priors/gibbs.py
@@ -14,3 +14,19 @@ def get_priors_params_uniform_priors_invgamma_sigmac_int2(alpha: float = 0.003, 
     prior_params_dict['c']['alpha_prior'] = alpha
     prior_params_dict['c']['beta_prior'] = beta
     return prior_params_dict
+
+def get_priors_params_uniform_invgamma(alpha: float = 0.003, beta: float = 0.003):
+    prior_params_dict = get_priors_params_uniform_priors()
+    # sigma_int2
+    prior_params_dict['M']['alpha_prior'] = alpha
+    prior_params_dict['M']['beta_prior'] = beta
+    # sigmac_int2
+    prior_params_dict['c']['alpha_prior'] = alpha
+    prior_params_dict['c']['beta_prior'] = beta
+    # sigmax2
+    prior_params_dict['x']['alpha_prior'] = alpha
+    prior_params_dict['x']['beta_prior'] = beta
+    # tau
+    prior_params_dict['tau']['alpha_prior'] = alpha
+    prior_params_dict['tau']['beta_prior'] = beta
+    return prior_params_dict
