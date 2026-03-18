fd2d5d2239d89b274c54e5c329d4c0c3ca2b0704
Marco Giunta <72098193+marco-giunta@users.noreply.github.com>
2026-03-18 19:33:02 +0100

    shorten labels

--- scripts/ztf_foundation_gibbs_posterior_cornerplot.py
+++ scripts/ztf_foundation_gibbs_posterior_cornerplot.py
@@ -33,7 +33,7 @@ def ztf_foundation_gibbs_posterior_cornerplot(argv = None):
 
     print('Plotting complete cornerplot...')
     fig = simplebayesn.visualize.compare_posterior_cornerplots([gd_ztf, gd_fnd], start_idx = 1000,
-                                                               labels = ['ZTF HQ VL', 'Foundation (cosmo + no cosmo samples)'],
+                                                               labels = ['ZTF HQ VL', 'Foundation (cosmo + no cosmo)'],
                                                                contours_colors = ['C1', 'C0'],
                                                                mean_colors = ['C1', 'C0'])
     fig.savefig(fig_path / Path('ztf_fnd_cornerplot.pdf'))
@@ -41,7 +41,7 @@ def ztf_foundation_gibbs_posterior_cornerplot(argv = None):
     print('Plotting color cornerplot...')
     pp = ['tau', 'RB', 'c0_int', 'sigmac_int', 'beta_int']
     fig = simplebayesn.visualize.compare_posterior_cornerplots([gd_ztf, gd_fnd], start_idx = 1000, params_to_plot = pp,
-                                                               labels = ['ZTF HQ VL', 'Foundation (cosmo + no cosmo samples)'],
+                                                               labels = ['ZTF HQ VL', 'Foundation (cosmo + no cosmo)'],
                                                                contours_colors = ['C1', 'C0'], mean_colors = ['C1', 'C0'])
     fig.savefig(fig_path / Path('ztf_fnd_color_params_cornerplot.pdf'))
 
